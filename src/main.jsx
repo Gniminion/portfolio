768d8446a36fdcb19b6e64accfca27799971eed2
MingMing Z <mingmingzhangcd@gmail.com>
2026-05-15 17:15:36 -0400

    art page routing issue

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if (sessionStorage.redirect) {
+  window.history.replaceState(null, null, sessionStorage.redirect);
+  delete sessionStorage.redirect;
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
